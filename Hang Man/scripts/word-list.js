32f0bb09a1ade58e6f1af50aeb53d560a17442f1
Gowthamjagadeesan <117513699+Gowthamjagadeesan@users.noreply.github.com>
2024-03-21 17:31:24 +0530

    Add files via upload

---
+++ Hang Man/scripts/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word:"guitar",
+        hint :"A musical instrument with strings"
+    },
+    {
+        word:"oxygen",
+        hint:"A colorless, odourless gas essential for Life"
+    },
+    {
+        word:"mind",
+        hint:"Travels faster than light."
+    },
+    {
+        word:"love",
+        hint:"Everyone will have this but not share to everyone!!!"
+    },
+    {
+        word:"love",
+        hint:"Everyone will have this but not share to everyone!!!"
+    }
+];
